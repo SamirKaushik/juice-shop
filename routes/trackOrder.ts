af996d93245882810b0b8bab52f3177020ce9402
Samir Kaushik <74591539+SamirKaushik@users.noreply.github.com>
2024-07-07 23:39:41 +0530

    Update trackOrder.ts

--- routes/trackOrder.ts
+++ routes/trackOrder.ts
@@ -14,15 +14,6 @@ module.exports = function trackOrder () {
     const id = !utils.isChallengeEnabled(challenges.reflectedXssChallenge) ? String(req.params.id).replace(/[^\w-]+/g, '') : req.params.id
 
     challengeUtils.solveIf(challenges.reflectedXssChallenge, () => { return utils.contains(id, '<iframe src="javascript:alert(`xss`)">') })
-    db.ordersCollection.find({ $where: `this.orderId === '${id}'` }).then((order: any) => {
-      const result = utils.queryResultToJson(order)
-      challengeUtils.solveIf(challenges.noSqlOrdersChallenge, () => { return result.data.length > 1 })
-      if (result.data[0] === undefined) {
-        result.data[0] = { orderId: id }
-      }
-      res.json(result)
-    }, () => {
-      res.status(400).json({ error: 'Wrong Param' })
-    })
+    
   }
 }
